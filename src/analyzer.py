c11ac152bcd40e50ecd8493be13e548822e79f42
imarmaanshalik <armaanshalike@gmail.com>
2026-09-06 19:45:37 +0530

    Add skill analysis function for resumes and jobs

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,1 +1,17 @@
+def analyze_skills(resume_skills, job_skills):
 
+    resume_set = set(resume_skills)
+    job_set = set(job_skills)
+
+    matched = sorted(resume_set.intersection(job_set))
+    missing = sorted(job_set - resume_set)
+
+    if job_set:
+        skill_score = round(
+            (len(matched) / len(job_set)) * 100,
+            2
+        )
+    else:
+        skill_score = 0
+
+    return matched, missing, skill_score
